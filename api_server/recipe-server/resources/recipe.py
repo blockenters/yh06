82a6fe4667b2251554ebce94ea1b139455c72fb2
blockenters <blockenters@gmail.com>
2024-05-21 12:19:37 +0900

    Update recipe.py

--- api_server/recipe-server/resources/recipe.py
+++ api_server/recipe-server/resources/recipe.py
@@ -47,5 +47,55 @@ class RecipeListResource(Resource) :
         
         return {'result' : 'success'}, 200
 
+    def get(self) :
 
+        # 1. 클라이언트가 보낸 데이터가 있으면
+        #    받아준다.
+
+        offset = request.args['offset']
+        limit = request.args['limit']
+
+        print(offset , limit)
+
+        # 2. DB로부터 데이터를 가져온다. 
+        try :
+            
+            connection = get_connection()
+
+            query = '''select *
+                        from recipe
+                        limit '''+offset+''', '''+limit+''';'''
+            # record = ()
+
+            cursor = connection.cursor(dictionary=True)
+
+            cursor.execute(query)
+
+            result_list = cursor.fetchall()
+
+            print(result_list)
+
+            cursor.close()
+            connection.close()
+
+        except Error as e:
+            if cursor is not None:
+                cursor.close()
+            if connection is not None:
+                connection.close()
+            return {'result' : 'fail', 'error' : str(e)}, 500      
+
+        # 3. 클라이언트에 json 만들어서 응답한다.       
+        i = 0
+        for row in result_list :
+            result_list[i]['created_at'] = row['created_at'].isoformat()
+            result_list[i]['updated_at'] = row['updated_at'].isoformat()
+            i = i + 1
+
+        print()
+        print(result_list)
+
+        return { 'items' : result_list,
+                'count' : len(result_list),
+                  'result' : 'success'  }
 
