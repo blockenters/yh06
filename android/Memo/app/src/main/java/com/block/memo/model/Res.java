b680a9e25847b819a5ebefce53d7851e6924c8d6
blockenters <blockenters@gmail.com>
2024-06-14 15:09:23 +0900

    add

--- android/Memo/app/src/main/java/com/block/memo/model/Res.java
+++
@@ -1,6 +0,0 @@
-package com.block.memo.model;
-
-public class Res {
-
-    public String result;
-}
--- android/Memo/app/src/main/java/com/block/memo/model/UserRes.java
+++
@@ -1,7 +0,0 @@
-package com.block.memo.model;
-
-public class UserRes {
-
-    public String result;
-    public String accessToken;
-}
